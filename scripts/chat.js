699d28d7514dfccf8bb613eee31f742b1005d056
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-04 19:58:13 +0545

    Add real time listeners to get chats

--- scripts/chat.js
+++ scripts/chat.js
@@ -18,10 +18,24 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    // Setup a real time listener
+    getChats(callback){
+        this.chats
+            .onSnapshot(snapshot =>{
+                // use docChanges() to get all changed document
+                snapshot.docChanges().forEach(change =>{
+                    if(change.type === 'added'){    // added for saved document, and removed for deleted document
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
 }
 
 // Create a new instance of our Chatroom
 const chatroom = new Chatroom('golang' , 'viv-bhai');
-chatroom.addChat('hello gophers')
-    .then(()=>console.log("Chat added to firestore")
-    ).catch(err => console.log(err));
+
+// Call getChats 
+chatroom.getChats((data) =>{
+    console.log(data);
+});
