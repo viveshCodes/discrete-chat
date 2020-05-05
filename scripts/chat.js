4ae69f9f1eaf902bc42f74f0e991a9c9064d2c89
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-05 11:17:04 +0545

    Revert "Feature to update username and room"
    This reverts commit e9130799cffe215335dc46612d3d8ce52ad18a56.

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,10 +1,9 @@
-/* 1: Add new chat document */
 class Chatroom {
     constructor(room , username){
         this.room = room;
         this.username = username;
-        this.chats = db.collection('chats');        // Store refernce to our collection : chats
-        this.unsub;
+        // Store refernce to our collection : chats
+        this.chats = db.collection('chats');
     }
     // Async Method To Add Chat
     async addChat(message){
@@ -19,34 +18,24 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
-    /* 2 :Setup a real time listener */
+    // Setup a real time listener
     getChats(callback){
-        console.log("got a call")
-       this.unsub= this.chats
+        this.chats
             .where('room','==', this.room)
-            .orderBy('created_at')                           // To use this property, we need to enable indexing for room
+            .orderBy('created_at')        // To use this property, we need to enable indexing for room
             .onSnapshot(snapshot =>{
-                snapshot.docChanges().forEach(change =>{     // use docChanges() to get all changed document
-                    if(change.type === 'added'){            // added for saved document, and removed for deleted document
+                // use docChanges() to get all changed document
+                snapshot.docChanges().forEach(change =>{
+                    if(change.type === 'added'){    // added for saved document, and removed for deleted document
                         callback(change.doc.data());
                     }
                 });
             });
-    } // end getChats()
-
-    updateUsername(username){
-        this.username = username;
-    }
-    updateRoom(room){
-        this.room = room;
-        console.log("room update");
-        if(this.unsub){
-            this.unsub();
-        }       
     }
 }
 
 
+<<<<<<< HEAD
 // setTimeout(() =>{
 //     chatroom.updateRoom("firebase");
 //     chatroom.updateUsername("viv");
@@ -55,3 +44,9 @@ class Chatroom {
 //     }); 
 //     chatroom.addChat('Hello');
 // });
+=======
+// Call getChats 
+chatroom.getChats((data) =>{
+    console.log(data);
+});
+>>>>>>> parent of e913079... Feature to update username and room
