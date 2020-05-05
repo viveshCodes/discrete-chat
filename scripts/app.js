b0af5111880c9356888b7443c6f1876300c4c3b6
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-05 16:09:11 +0545

    Dynamically upate username

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // DOM queries
 const chatlist = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm =document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 // add a new chat
 newChatForm.addEventListener('submit', event =>{
@@ -12,6 +14,21 @@ newChatForm.addEventListener('submit', event =>{
         .then(()=>{                               // since async function returns promise so we used .then                                  
             newChatForm.reset()
         }).catch(err => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', event => {
+    event.preventDefault();
+    // update username
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateUsername(newName);
+    // reset form
+    newNameForm.reset();
+    // show notification for 
+    updateMssg.innerText = `Username updated to ${newName}`;
+    setTimeout(() =>{
+        updateMssg.innerText = '';
+    }, 3000);
 });
 
 // class instances 
--- scripts/chat.js
+++ scripts/chat.js
@@ -1,9 +1,10 @@
+/* 1: Add new chat document */
 class Chatroom {
     constructor(room , username){
         this.room = room;
         this.username = username;
-        // Store refernce to our collection : chats
-        this.chats = db.collection('chats');
+        this.chats = db.collection('chats');        // Store refernce to our collection : chats
+        this.unsub;
     }
     // Async Method To Add Chat
     async addChat(message){
@@ -18,19 +19,30 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
-    // Setup a real time listener
+    /* 2 :Setup a real time listener */
     getChats(callback){
-        this.chats
+       this.unsub= this.chats
             .where('room','==', this.room)
-            .orderBy('created_at')        // To use this property, we need to enable indexing for room
+            .orderBy('created_at')                           // To use this property, we need to enable indexing for room
             .onSnapshot(snapshot =>{
-                // use docChanges() to get all changed document
-                snapshot.docChanges().forEach(change =>{
-                    if(change.type === 'added'){    // added for saved document, and removed for deleted document
+                snapshot.docChanges().forEach(change =>{     // use docChanges() to get all changed document
+                    if(change.type === 'added'){            // added for saved document, and removed for deleted document
                         callback(change.doc.data());
                     }
                 });
             });
+    } // end getChats()
+
+    updateUsername(username){
+        this.username = username;
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log("room update");
+        if(this.unsub){
+            this.unsub();
+        }       
     }
 }
+
 
