30645edeaf107e872c699768377a467c85e414cc
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-05 11:43:21 +0545

    send new chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,18 @@
 // DOM queries
 const chatlist = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// add a new chat
+newChatForm.addEventListener('submit', event =>{
+    event.preventDefault();
+    const message = newChatForm.message.value.trim(); // message = id of input field, trim is to remove white spaces
+    
+    // Add new message to the database 
+    chatroom.addChat(message)
+        .then(()=>{                               // since async function returns promise so we used .then                                  
+            newChatForm.reset()
+        }).catch(err => console.log(err));
+});
 
 // class instances 
 const chatroom = new Chatroom('golang' , 'viv-bhai');
